a9c9256bcfdfb18a4944faeb475557f246a92354
pgoggles <praneeth.gogineni@gmail.com>
2021-04-19 10:25:40 -0700

    Add Answer Helpfulness & Improve CSS Styling Reusability

--- client/src/components/QA/AnswerRenderer.jsx
+++ client/src/components/QA/AnswerRenderer.jsx
@@ -8,18 +8,24 @@ const AnswerRenderer = (props) => {
     answererName: [],
     answerBody: [],
     answerDate: [],
+    answerHelpfulness: [],
+    answerID: [],
   };
   for (let i = 0; i < Object.keys(answers).length; i += 1) {
     const currentAnswer = answers[Object.keys(answers)[i]];
     answersObject.answererName.push(currentAnswer.answerer_name);
     answersObject.answerBody.push(currentAnswer.body);
     answersObject.answerDate.push(currentAnswer.date);
+    answersObject.answerHelpfulness.push(currentAnswer.helpfulness);
+    answersObject.answerID.push(currentAnswer.id);
   }
 
   const parseAnswers = (answerBody, i) => {
     const answerDate = answersObject.answerDate[i];
     const answererName = answersObject.answererName[i];
+    const answerHelpfulness = answersObject.answerHelpfulness[i];
     return (
+<<<<<<< HEAD
       <styles.AnswerText>
         {answerBody}
         {' '}
@@ -27,6 +33,35 @@ const AnswerRenderer = (props) => {
         {' '}
         {answererName}
       </styles.AnswerText>
+=======
+      <div>
+        <styles.AnswerText>
+          <styles.BoldedText>
+            A:
+            {' '}
+          </styles.BoldedText>
+          {answerBody}
+        </styles.AnswerText>
+        <styles.AnswerSubtitle>
+          by
+          {' '}
+          {answererName}
+          ,
+          {' '}
+          {moment(answerDate).fromNow()}
+          {'  |  '}
+          Helpful?
+          {' '}
+          <styles.HyperLink href="http://google.com">Yes</styles.HyperLink>
+          {' '}
+          (
+          {answerHelpfulness}
+          )
+          {'  |  '}
+          <styles.HyperLink href="http://google.com">Report</styles.HyperLink>
+        </styles.AnswerSubtitle>
+      </div>
+>>>>>>> Add Answer Helpfulness & Improve CSS Styling Reusability
     );
   };
   return (
--- client/src/components/QA/styles.js
+++ client/src/components/QA/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+<<<<<<< HEAD
 const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
@@ -17,6 +18,55 @@ const QuestionText = styled.div`
   font-weight: 400;
   font-style: Italic;
   color: black;
+=======
+const primarySize = '1.1em';
+const secondarySize = '0.75em';
+const primaryColor = '#454444';
+const secondaryColor = '#858383';
+const primaryFont = 'Helvetica Neue';
+
+const QA = styled.div`
+  margin-left: 40%;
+`;
+
+const Title = styled.div`
+  margin-top: 5px;
+  font-family: ${primaryFont};
+  font-weight: 200;
+  color: ${primaryColor};
+  font-size: ${primarySize};
+`;
+
+const BoldedText = styled.div`
+  font-weight: 450;
+  font-style: Bold;
+  padding-left: 5px;
+  padding-right: 5px;
+  text-align: left;
+`;
+
+const QuestionBlock = styled.div`
+  font-family: ${primaryFont};
+  margin-top:5px;
+  margin-bottom: 10px;
+  font-size: ${primarySize};
+  color: ${primaryColor};
+  width: 50%;
+`;
+const QuestionLine = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const QuestionText = styled.div`
+  margin-bottom: 15px;
+`;
+
+const QuestionSubtitle = styled.div`
+  text-align: right;
+  font-size: ${secondarySize};
+  color: #858383;
+>>>>>>> Add Answer Helpfulness & Improve CSS Styling Reusability
 `;
 
 const AnswerText = styled.div`
@@ -25,6 +75,7 @@ const AnswerText = styled.div`
   color: black;
 `;
 
+<<<<<<< HEAD
 const QuestionBlock = styled.div`
   border: solid 1px;
   margin-bottom: 10px;
@@ -36,3 +87,40 @@ const AnswerBlock = styled.div`
 
 const styles = { Title, Title2, QuestionText, AnswerText, QuestionBlock, AnswerBlock };
 export default styles;
+=======
+const AnswerSubtitle = styled.div`
+  padding-top: 2px;
+  padding-left: 32px;
+  margin-bottom: 8px;
+  font-size: ${secondarySize};
+  color: #858383;
+`;
+
+const AnswerBlock = styled.div`
+`;
+
+const SearchBar = styled.input`
+  width: 40%;
+  margin-top: 5px;
+`;
+
+const HyperLink = styled.a`
+  color: ${secondaryColor};
+`;
+
+const styles = {
+  Title,
+  BoldedText,
+  QuestionBlock,
+  QuestionLine,
+  QuestionText,
+  QuestionSubtitle,
+  AnswerText,
+  AnswerSubtitle,
+  AnswerBlock,
+  QA,
+  SearchBar,
+  HyperLink,
+};
+export default styles;
+>>>>>>> Add Answer Helpfulness & Improve CSS Styling Reusability

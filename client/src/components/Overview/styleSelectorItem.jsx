0a271f5b61d14a7c974901721046f52fb55aa932
calvin <calvinchenhupro@gmail.com>
2021-04-22 20:39:17 -0700

    update

--- client/src/components/Overview/styleSelectorItem.jsx
+++ client/src/components/Overview/styleSelectorItem.jsx
@@ -24,7 +24,7 @@ const StyleSelectorItem = class extends React.Component {
 
   handleClick() {
     const photos = this.props.item;
-    console.log(this.props.item);
+    // console.log(this.props.item);
     this.props.selectStyle(photos);
     this.props.addOutfit(photos);
   }

5fc2f2b1c37bc836fcc02eff07f86d86bf47c7d2
calvin <calvinchenhupro@gmail.com>
2021-04-25 15:44:06 -0700

    update add to cart

--- client/src/components/Overview/addToCart/addToCart.jsx
+++ client/src/components/Overview/addToCart/addToCart.jsx
@@ -25,7 +25,7 @@ const AddToCart = class extends React.Component {
   render() {
     return (
       <div className="add-to-cart">
-        <div>select size</div>
+
         <select
           className="select-size"
           onChange={(e) => {
@@ -34,6 +34,7 @@ const AddToCart = class extends React.Component {
             });
           }}
         >
+          <option value="" disabled selected hidden>select size</option>
           <option value="XS">XS</option>
           <option value="S">S</option>
           <option value="M">M</option>
@@ -50,12 +51,22 @@ const AddToCart = class extends React.Component {
             });
           }}
         >
+          <option value="" disabled selected hidden>select quantity</option>
           <option value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
           <option value="4">4</option>
           <option value="5">5</option>
           <option value="6">6</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+          <option value="10">10</option>
+          <option value="11">11</option>
+          <option value="12">12</option>
+          <option value="13">13</option>
+          <option value="14">14</option>
+          <option value="15">15</option>
         </select>
         <br />
 

756a9d8b21984ba417c41a0ef5f932ef577387cd
Praneeth Gogineni <praneeth.gogineni@gmail.com>
2021-04-25 18:12:49 -0700

    Reimplement how Answers are parsed and rendered

--- client/src/components/QA/SubComponents/AnswerRenderer.jsx
+++ client/src/components/QA/SubComponents/AnswerRenderer.jsx
@@ -13,50 +13,36 @@ class AnswerRenderer extends React.Component {
   constructor(props) {
     super(props);
     const { answers } = props;
-    this.answersObject = {
-      answererName: [],
-      answerBody: [],
-      answerDate: [],
-      answerHelpfulness: [],
-      answerID: [],
-      answerReport: [],
-    };
+    const answersArray = [];
     for (let i = 0; i < Object.keys(answers).length; i += 1) {
       const currentAnswer = answers[Object.keys(answers)[i]];
-      this.answersObject.answererName.push(currentAnswer.answerer_name);
-      this.answersObject.answerBody.push(currentAnswer.body);
-      this.answersObject.answerDate.push(currentAnswer.date);
-      this.answersObject.answerHelpfulness.push(currentAnswer.helpfulness);
-      this.answersObject.answerID.push(currentAnswer.id);
-      this.answersObject.answerReport.push('Report');
+      currentAnswer.answerReport = 'Report';
+      answersArray.push(currentAnswer);
     }
     this.state = {
-      answersObject: this.answersObject,
+      answersArray,
       showAllAnswers: false,
     };
   }
 
   AnswerHelpful(answerID, identifier) {
     AnswerHelpfulPost(answerID, () => {
-      const { answersObject } = this.state;
-      const currentHelpfulness = answersObject.answerHelpfulness[identifier];
-      answersObject.answerHelpfulness[identifier] = currentHelpfulness + 1;
-      this.setState({ answersObject });
+      const { answersArray } = this.state;
+      const currentHelpfulness = answersArray[identifier].helpfulness;
+      answersArray[identifier].helpfulness = currentHelpfulness + 1;
+      this.setState({ answersArray });
     });
   }
 
   AnswerReport(answerID, identifier) {
     AnswerReport(answerID, () => {
-      const { answersObject } = this.state;
-      answersObject.answerReport[identifier] = 'Reported';
-      this.setState({ answersObject });
+      const { answersArray } = this.state;
+      answersArray[identifier].answerReport = 'Reported';
+      this.setState({ answersArray });
     });
   }
 
-  parseAnswers(answerBody, i) {
-    const answerDate = this.state.answersObject.answerDate[i];
-    const answererName = this.state.answersObject.answererName[i];
-    const answerHelpfulness = this.state.answersObject.answerHelpfulness[i];
+  parseAnswers(answer, identifier) {
     return (
       <div key={Math.random() * 100000}>
         <styles.AnswerText data-testid="AnswerText">
@@ -64,34 +50,34 @@ class AnswerRenderer extends React.Component {
             A:
             {' '}
           </styles.BoldedText>
-          {answerBody}
+          {answer.body}
         </styles.AnswerText>
         <styles.AnswerSubtitle>
           by
           {' '}
-          {answererName}
+          {answer.answerer_name}
           ,
           {' '}
-          {moment(answerDate).format('MMMM DD, YYYY')}
+          {moment(answer.date).format('MMMM DD, YYYY')}
           {'  |  '}
           Helpful?
           {' '}
           <styles.HyperLink onClick={
-            () => this.AnswerHelpful.bind(this)(this.state.answersObject.answerID[i], i)
+            () => this.AnswerHelpful.bind(this)(answer.id, identifier)
             }
           >
             Yes
           </styles.HyperLink>
           {' '}
           (
-          {answerHelpfulness}
+          {answer.helpfulness}
           )
           {'  |  '}
           <styles.HyperLink onClick={
-            () => this.AnswerReport.bind(this)(this.state.answersObject.answerID[i], i)
+            () => this.AnswerReport.bind(this)(answer.id, identifier)
             }
           >
-            {this.state.answersObject.answerReport[i]}
+            {answer.answerReport}
           </styles.HyperLink>
         </styles.AnswerSubtitle>
       </div>
@@ -108,22 +94,22 @@ class AnswerRenderer extends React.Component {
         { this.state.showAllAnswers
           ? (
             <div>
-              {this.state.answersObject.answerBody.map(
-                (answerBody, i) => this.parseAnswers(answerBody, i),
+              {this.state.answersArray.map(
+                (answer, i) => this.parseAnswers(answer, i),
               )}
             </div>
           )
           : (
             <div>
-              {this.state.answersObject.answerBody.slice(0, 2).map(
-                (answerBody, i) => this.parseAnswers(answerBody, i),
+              {this.state.answersArray.slice(0, 2).map(
+                (answer, i) => this.parseAnswers(answer, i),
               )}
             </div>
           )}
-        { this.state.showAllAnswers || this.state.answersObject.answerBody.length < 3
+        { this.state.showAllAnswers || this.state.answersArray.length < 3
           ? <div />
           : <styles.LoadMoreAnswers onClick={this.showAllAnswers.bind(this)} data-testid="LoadMoreAnswers">LOAD MORE ANSWERS</styles.LoadMoreAnswers>}
-        {this.state.answersObject.answerBody.length === 0
+        {this.state.answersArray.length === 0
           ? <styles.AnswerText data-testid="NoAnswer"> No Answers Available for this Question.</styles.AnswerText>
           : <div />}
       </div>
